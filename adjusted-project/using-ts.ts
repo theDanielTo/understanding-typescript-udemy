51bcefcc4a1540d6aa9c1d2786d3641031ac6eda
theDanielTo <dto1989@gmail.com>
2021-09-02 16:52:21 -0700

    core types. quit 1.

--- adjusted-project/using-ts.ts
+++ adjusted-project/using-ts.ts
@@ -1,11 +1,13 @@
 const button = document.querySelector("button");
 const input1 = document.getElementById("num1")! as HTMLInputElement;
 const input2 = document.getElementById("num2")! as HTMLInputElement;
+// ! means "never null"
+// HTML => type casting
 
-function add(num1: number, num2: number) {
+function add(num1: number, num2: number) { // parameters are strong typed
   return num1 + num2;
 }
 
 button.addEventListener("click", function() {
-  console.log(add(+input1.value, +input2.value));
+  console.log(add(+input1.value, +input2.value)); // + converts to 'Number'
 });
