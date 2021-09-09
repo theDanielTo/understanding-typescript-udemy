d940fd9b0461e3de54401c5cf6e5bab93652f9e2
theDanielTo <dto1989@gmail.com>
2021-09-09 11:38:30 -0700

    completed section 3

--- section-3/src/app.ts
+++ section-3/src/app.ts
@@ -1,8 +1,5 @@
 // tsc --init => tsconfig.json file
 // tsc -w => watch mode
-// const userName = "Daniel To!";
-
-// console.log(userName);
 
 const $btn = document.querySelector('button');
 $btn?.addEventListener('click', () => {
