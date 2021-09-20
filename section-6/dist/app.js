220051b5f21c7de6ce3919d43b94ccefb403ff06
theDanielTo <dto1989@gmail.com>
2021-09-20 11:46:18 -0700

    type guards

--- section-6/dist/app.js
+++ section-6/dist/app.js
@@ -4,3 +4,49 @@ var e1 = {
     privileges: ['create-server'],
     startDate: new Date()
 };
+function add(a, b) {
+    if (typeof a === 'string' || typeof b === 'string') {
+        return a.toString() + b.toString();
+    }
+    return a + b;
+}
+function printEmployeeInformation(emp) {
+    console.log('Name: ' + emp.name);
+    if ('privileges' in emp) {
+        console.log('Privileges: ' + emp.privileges);
+    }
+    if ('startDate' in emp) {
+        console.log('Start Date: ' + emp.startDate);
+    }
+}
+printEmployeeInformation(e1);
+printEmployeeInformation({ name: 'employee3', privileges: ['bathroom'] });
+var Car = (function () {
+    function Car() {
+    }
+    Car.prototype.drive = function () {
+        console.log('Driving...');
+    };
+    return Car;
+}());
+var Truck = (function () {
+    function Truck() {
+    }
+    Truck.prototype.drive = function () {
+        console.log('Driving a truck...');
+    };
+    Truck.prototype.loadCargo = function (amount) {
+        console.log('Loading cargo...' + amount);
+    };
+    return Truck;
+}());
+var v1 = new Car();
+var v2 = new Truck();
+function useVehicle(vehicle) {
+    vehicle.drive();
+    if (vehicle instanceof Truck) {
+        vehicle.loadCargo(500);
+    }
+}
+useVehicle(v1);
+useVehicle(v2);
--- section-6/src/app.ts
+++ section-6/src/app.ts
@@ -20,3 +20,57 @@ type Combinable = string | number;
 type Numberic = number | boolean;
 
 type Universal = Combinable & Numberic; // intersection ==> number
+
+function add(a: Combinable, b: Combinable) {
+  // type guard. if string, then concatenate. if number, arithmetic
+  if (typeof a === 'string' || typeof b === 'string') {
+    return a.toString() + b.toString();
+  }
+  return a + b;
+}
+
+type UnknownEmployee = Employee | Admin;
+
+function printEmployeeInformation(emp: UnknownEmployee) {
+  console.log('Name: ' + emp.name);
+  if ('privileges' in emp) { // checking if a prop is in an obj
+    console.log('Privileges: ' + emp.privileges);
+  }
+  if ('startDate' in emp) {
+    console.log('Start Date: ' + emp.startDate);
+  }
+}
+
+printEmployeeInformation(e1);
+printEmployeeInformation({ name: 'employee3', privileges: ['bathroom'] });
+
+class Car {
+  drive() {
+    console.log('Driving...');
+  }
+}
+
+class Truck {
+  drive() {
+    console.log('Driving a truck...');
+  }
+
+  loadCargo(amount: number) {
+    console.log('Loading cargo...' + amount);
+  }
+}
+
+type Vehicle = Car | Truck;
+
+const v1 = new Car();
+const v2 = new Truck();
+
+function useVehicle(vehicle: Vehicle) {
+  vehicle.drive();
+  if (vehicle instanceof Truck) { // another way to check for prop (type guard)
+    vehicle.loadCargo(500);
+  }
+}
+
+useVehicle(v1);
+useVehicle(v2);
