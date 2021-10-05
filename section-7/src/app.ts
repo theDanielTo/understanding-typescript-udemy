48fc8761801f9c7288b64bf3c701578b7a8359c3
theDanielTo <dto1989@gmail.com>
2021-10-05 13:02:57 -0700

    working with constraints

--- section-7/src/app.ts
+++ section-7/src/app.ts
@@ -16,9 +16,11 @@
 // object is a highly unspecific type
 // T and U can be of any type.
 // TS can now see that merge returns intersection of T & U
-function merge<T, U>(objA: T, objB: U) {
+// T extends object => adds constraints (Object.assign() requires 2 objects)
+function merge<T extends object, U extends object>(objA: T, objB: U) {
   return Object.assign(objA, objB); // merge 2 objects into 1
 }
 
 const mergedObj = merge({ name: "Daniel", hobbies: ['Sports'] }, { age: 32 });
+// const mergedObj2 = merge({ name: "Daniel", hobbies: ['Sports'] }, 32); // 32 is not an object. silently fails
 console.log(mergedObj);
