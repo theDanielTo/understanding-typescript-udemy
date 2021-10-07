a9b4629ab1b47f05d2b3ce10d9144ed7d82805e2
theDanielTo <dto1989@gmail.com>
2021-10-07 14:21:06 -0700

    decorator factories

--- section-8/src/app.ts
+++ section-8/src/app.ts
@@ -1,9 +1,11 @@
-function Logger(constructor: Function) {
-  console.log('Logging...');
-  console.log(constructor);
+function Logger(logString: string) {
+  return function(constructor: Function) {
+    console.log(logString);
+    console.log(constructor);
+  }
 }
 
-@Logger // Person class using Logger decorator
+@Logger('LOGGING - PERSON') // Person class using Logger decorator
 class Person {
   name = 'Daniel';
 
