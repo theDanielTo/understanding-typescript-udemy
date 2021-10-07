d974c2e6b55f49b7ea207a291e981f265cb004f8
theDanielTo <dto1989@gmail.com>
2021-10-07 14:59:49 -0700

    multiple decorators

--- section-8/src/app.ts
+++ section-8/src/app.ts
@@ -1,11 +1,30 @@
 function Logger(logString: string) {
+  console.log('LOGGER FACTORY');
+
   return function(constructor: Function) {
     console.log(logString);
     console.log(constructor);
   }
 }
 
-@Logger('LOGGING - PERSON') // Person class using Logger decorator
+function WithTemplate(template: string, hookId: string) {
+  console.log('TEMPLATE FACTORY');
+  // if not interested in using the argument (constructor). specify with under_score to tell TS that you are aware
+  return function (constructor: any) {
+    console.log('Rendering template.');
+
+    const hookEl = document.getElementById(hookId);
+    const p = new constructor();
+    if (hookEl) {
+      hookEl.innerHTML = template;
+      hookEl.querySelector('h1')!.textContent = p.name;
+    }
+  };
+}
+
+// @Logger('LOGGING - PERSON') // Person class using Logger decorator
+@Logger('Logging') // code outside of return function runs top to bottom
+@WithTemplate('<h1>My Person Object</h1>', 'app') // with multiple decorators, decorator (return) function run bottom up
 class Person {
   name = 'Daniel';
 
