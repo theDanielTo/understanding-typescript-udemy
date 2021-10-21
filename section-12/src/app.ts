9e671eb9a91892400ee52cc7b496011ba0e4f03a
theDanielTo <dto1989@gmail.com>
2021-10-21 13:34:29 -0700

    validate class objects with class-validator (validate fn).

--- section-12/src/app.ts
+++ section-12/src/app.ts
@@ -1,5 +1,6 @@
 import "reflect-metadata";
 import { plainToClass } from "class-transformer";
+import { validate } from "class-validator";
 import { Product } from "./product.model";
 
 // import _ from 'lodash';
@@ -13,6 +14,16 @@ const products = [
   { title: 'A Carpet', price: 29.99 },
   { title: 'A Book', price: 10.00 }
 ];
+
+const newProd = new Product('', -5.99);
+validate(newProd).then(errors => {
+  if (errors.length > 0) {
+    console.log('VALIDATION ERRORS!');
+    console.log(errors);
+  } else {
+    console.log(newProd.getInformation());
+  }
+});
 
 // const loadedProducts = products.map(prod => {
 //   return new Product(prod.title, prod.price);
