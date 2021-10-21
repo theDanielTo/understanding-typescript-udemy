12b3106befb9ed6412424b58b2692fc05c9cd4b4
theDanielTo <dto1989@gmail.com>
2021-10-21 12:19:51 -0700

    using plainToClass from class-transformer library

--- section-12/src/app.ts
+++ section-12/src/app.ts
@@ -1,7 +1,28 @@
-import _ from 'lodash';
+import "reflect-metadata";
+import { plainToClass } from "class-transformer";
+import { Product } from "./product.model";
+
+// import _ from 'lodash';
+// console.log(_.shuffle([1, 2, 3]));
 
 // declare var GLOBAL: string;
-
-console.log(_.shuffle([1, 2, 3]));
-
 // console.log(GLOBAL);
+
+
+const products = [
+  { title: 'A Carpet', price: 29.99 },
+  { title: 'A Book', price: 10.00 }
+];
+
+// const loadedProducts = products.map(prod => {
+//   return new Product(prod.title, prod.price);
+// });
+const loadedProducts = plainToClass(Product, products);
+
+for (const prod of loadedProducts) {
+  console.log(prod.getInformation());
+}
+
+
+// const p1 = new Product('A Book', 12.99);
+// console.log(p1.getInformation());
