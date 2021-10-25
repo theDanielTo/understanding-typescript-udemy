66422c8c0729d7ec6809ac7579392ea4128ebfc1
theDanielTo <dto1989@gmail.com>
2021-10-25 11:26:22 -0700

    remove more unnecessary files/images. add TodoList.tsx component to render a todo list.

--- section-14/src/App.tsx
+++ section-14/src/App.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import TodoList from './components/TodoList';
 
-function App() {
+const App: React.FC = () => {
+  const todos = [{ id: "t1", text: "Finish the course" }];
   return (
     <div className="App">
-
+      {/* A component that adds todos */}
+      <TodoList items={todos}/>
     </div>
   );
 }
